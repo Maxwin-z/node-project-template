0e4a53fb0ba8703a4c6a1b4f0559824d6e7a6671
Maxwin-z <zwd2005@gmail.com>
2016-02-18 17:50:20 +0800

    add src folder

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import http from 'http';
+
+const port = 8080;
+http.createServer((req, res) => {
+    res.end('hello world!');
+}).listen(port, () => {
+    console.log('Server listen port:', port);
+});
