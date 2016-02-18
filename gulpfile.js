f0484089bee367e8fb1c8e2c001bed2f4d98f65c
Maxwin-z <zwd2005@gmail.com>
2016-02-18 17:59:33 +0800

    gulp task for babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
 'use strict';
 
 const gulp = require('gulp');
+const babel = require('gulp-babel');
+const watch = require('gulp-watch');
+const empty = require('gulp-empty');
+const yargs = require('yargs');
 
-gulp.task('default', () => {
-	console.log('hi');
-})
+if (yargs.nowatch) {
+    watch = empty;
+}
+
+gulp.task('babel', () => {
+    const path = './src/**/*.js*(x)';
+    return gulp.src(path)
+        .pipe(watch(path, {}))
+        .pipe(babel())
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task('default', ['babel']);
