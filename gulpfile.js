89a9598fde0877bc7caad546570b230592e38797
Maxwin-z <zwd2005@gmail.com>
2016-04-28 16:09:18 +0800

    add cross-env

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,26 @@
 'use strict';
 
 const gulp = require('gulp');
+const through = require('through2');
 const babel = require('gulp-babel');
-const empty = require('gulp-empty');
 const plumber = require('gulp-plumber');
 let watch = require('gulp-watch');
-const yargs = require('yargs');
 
-if (yargs.nowatch) {
-    watch = empty;
+if (!process.env.DEV) {
+  watch = function () {
+    return through.obj(function(file, enc, cb) {
+      cb(null, file);
+    })
+  };
 }
 
 gulp.task('babel', () => {
-    const path = './src/**/*.js*(x)';
-    return gulp.src(path)
-        .pipe(watch(path, {}))
-        .pipe(plumber())
-        .pipe(babel())
-        .pipe(gulp.dest("dist"));
+  const path = './src/**/*.js*(x)';
+  return gulp.src(path)
+    .pipe(watch(path, {}))
+    .pipe(plumber())
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
 });
 
 gulp.task('default', ['babel']);
